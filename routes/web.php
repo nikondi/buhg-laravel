5083119763d4153fd61a56e3eca57c1708735300
nikita <nikondi@narod.ru>
2025-02-19 02:34:36 +0300

    Push commit

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\WelcomeController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::middleware(['auth'])->group(function () {
     Route::get('/', WelcomeController::class)->name('welcome');
     Route::get('/logout', [AuthController::class, 'logout'])->name('login.logout');
