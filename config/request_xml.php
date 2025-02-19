bc5d8f4a57041652b42fea5d6b421497b2436e2b
nikita <nikondi@narod.ru>
2025-02-19 14:16:46 +0300

    Change xml code

--- config/request_xml.php
+++ config/request_xml.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'kod_one' => env('REQUEST_XML_KOD_ONE', '0000'),
+    'kod_one' => env('REQUEST_XML_KOD_ONE', '4800'),
     'kod_two' => env('REQUEST_XML_KOD_TWO', '5555'),
 ];
